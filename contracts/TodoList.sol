b034fa596d8cc465059e811e8c145bdcfaa14318
kaanakgundogdu <kaanakgundogdu@gmail.com>
2022-04-08 16:06:33 +0300

    App.js and index.html added

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,5 +2,22 @@ pragma solidity ^0.5.0;
 
 contract TodoList {
     // Code goes here...
-    uint public taskCount =0;
+    uint256 public taskCount = 0;
+
+    struct Task {
+        uint256 id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint256 => Task) public tasks;
+
+    constructor() public {
+        createTask("Check out dappuniversity.com");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
